8188277da82e7e4661a25d649adad2aca8f07705
KHAIRUL ISLAM <khairulislam38@gmail.com>
2023-07-11 13:46:50 +0600

    [Done] setup first slice, connect component with redux store

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "./redux/store";
+import { decrement, increment } from "./redux/features/counter/counterSlice";
+
 function App() {
-  return <h1 className="bg-red-500">Tech net</h1>;
+  const dispatch = useDispatch();
+  const { count } = useSelector((state: RootState) => state.counter);
+
+  return (
+    <div>
+      <div className="m-5 flex items-center gap-3">
+        <button
+          className="ring-2 ring-green-500 rounded p-1"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <div>{count}</div>
+        <button
+          className="ring-2 ring-red-500 rounded p-1"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
