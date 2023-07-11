5a148ee45993d637d3578e0ba421acd827d9320e
KHAIRUL ISLAM <khairulislam38@gmail.com>
2023-07-11 23:32:41 +0600

    [Done] devtool, middleware and custom middleware implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import { useDispatch, useSelector } from "react-redux";
-import { RootState } from "./redux/store";
 import {
   decrement,
   increment,
   incrementByAmount,
 } from "./redux/features/counter/counterSlice";
+import { useAppDispatch, useAppSelector } from "./redux/hooks";
 
 function App() {
-  const dispatch = useDispatch();
-  const { count } = useSelector((state: RootState) => state.counter);
+  const dispatch = useAppDispatch();
+  const { count } = useAppSelector((state) => state.counter);
 
   return (
     <div>
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./features/counter/counterSlice";
+import logger from "./middlewares/logger";
+// import { createLogger } from "redux-logger";
+
+// const logger = createLogger();
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  // devTools: process.env.NODE_ENV !== "production",
 });
 
 export type RootState = ReturnType<typeof store.getState>;
