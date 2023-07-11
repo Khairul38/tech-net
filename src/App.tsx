32b049ab2c031912187cdaf0008dc89e6524a260
KHAIRUL ISLAM <khairulislam38@gmail.com>
2023-07-12 01:07:51 +0600

    update frontend ui part

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,11 @@
-import {
-  decrement,
-  increment,
-  incrementByAmount,
-} from "./redux/features/counter/counterSlice";
-import { useAppDispatch, useAppSelector } from "./redux/hooks";
+import { Toaster } from './components/ui/Toaster';
+import MainLayout from './layouts/MainLayout';
 
 function App() {
-  const dispatch = useAppDispatch();
-  const { count } = useAppSelector((state) => state.counter);
-
   return (
     <div>
-      <div className="m-5 flex items-center gap-3">
-        <button
-          className="ring-2 ring-green-500 rounded p-1"
-          onClick={() => dispatch(increment())}
-        >
-          Increment
-        </button>
-        <button
-          className="ring-2 ring-green-500 rounded p-1"
-          onClick={() => dispatch(incrementByAmount(5))}
-        >
-          Increment by value
-        </button>
-        <div>{count}</div>
-        <button
-          className="ring-2 ring-red-500 rounded p-1"
-          onClick={() => dispatch(decrement())}
-        >
-          Decrement
-        </button>
-      </div>
+      <Toaster />
+      <MainLayout />
     </div>
   );
 }
